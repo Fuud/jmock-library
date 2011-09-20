63e324e7267b3871c26c8345ea01616069506412
Bobin Fedor <bobin@reksoft.ru>
2011-09-20 12:14:20 +0400

    1ю5 support

--- jmock-library/src/main/java/org/jmock/lib/action/RememberFirstParameterAction.java
+++ jmock-library/src/main/java/org/jmock/lib/action/RememberFirstParameterAction.java
@@ -6,12 +6,11 @@ import org.jmock.api.Invocation;
 
 public class RememberFirstParameterAction<T> implements Action {
     private final RememberParametersAction rpa = new RememberParametersAction();
-    @Override
+
     public Object invoke(Invocation invocation) throws Throwable {
        return rpa.invoke(invocation);
     }
 
-    @Override
     public void describeTo(Description description) {
         description.appendText("remeber first parameter");
     }
